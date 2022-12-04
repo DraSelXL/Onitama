e4fb9bcd005729053998fe9b691b1baa5f7bc790
DraSelXL <uni.nathanwill02@gmail.com>
2022-12-04 09:29:46 +0700

    Changed the overall structure of the PlayerColor class

--- Onitama/app/src/main/java/com/example/onitama/components/PlayerColor.kt
+++ Onitama/app/src/main/java/com/example/onitama/components/PlayerColor.kt
@@ -1,8 +1,10 @@
 package com.example.onitama.components
 
-class PlayerColor {
-    companion object {
-        var RED = "red"
-        var BLUE = "blue"
-    }
+/**
+ * An enum class that holds the value of the available player color.
+ * Has a color property that represents the string of the color name.
+ */
+enum class PlayerColor(val color: String) {
+    RED("red"),     // Indicates that the player color is red.
+    BLUE("blue")    // Indicates that the player color is blue.
 }
