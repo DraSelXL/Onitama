bfdfaf10a7516e9647562d4d8e513a8e698a7d07
DraSelXL <uni.nathanwill02@gmail.com>
2022-12-04 09:28:56 +0700

    Changed the overall structure of the PieceType class

--- Onitama/app/src/main/java/com/example/onitama/components/PieceType.kt
+++ Onitama/app/src/main/java/com/example/onitama/components/PieceType.kt
@@ -1,4 +1,11 @@
 package com.example.onitama.components
 
-enum class PieceType {
+/**
+ * An enum class representing all available type of a piece.
+ * Has a type property which holds the string value of the type.
+ *
+ */
+enum class PieceType(val type: String) {
+    PAWN("pawn"),       // Indicates that the piece type is a pawn piece.
+    MASTER("master")    // Indicates that the piece type is a master piece.
 }
