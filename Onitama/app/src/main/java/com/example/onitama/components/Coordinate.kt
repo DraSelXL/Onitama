57ea9bfe6c68ad1baf751ca95b9cce710ee74528
DraSelXL <uni.nathanwill02@gmail.com>
2022-12-04 09:27:50 +0700

    Switch the X and the Y axis of the coordinate class

--- Onitama/app/src/main/java/com/example/onitama/components/Coordinate.kt
+++ Onitama/app/src/main/java/com/example/onitama/components/Coordinate.kt
@@ -1,14 +1,13 @@
 package com.example.onitama.components
 
 /**
- * A class to hold coordinate in the board
+ * A class to hold coordinate values of the board.
  *
- * @param y Represents the Y axis of the board.
  * @param x Represents the X axis of the board.
+ * @param y Represents the Y axis of the board.
  */
 data class Coordinate(
     //ex: [0,1]
-    var y:Int,
-    var x:Int
-) {
-}
+    var x: Int,
+    var y: Int
+)
